6ea4ebf1e695e956e4eac336c4517b82c2b6f714
ydah <t.yudai92@gmail.com>
2026-09-20 08:46:32 +0900

    Use canonical editor demo source

--- demo/01_overview.rb
+++ demo/01_overview.rb
@@ -1,16 +1,12 @@
 # frozen_string_literal: true
 
 require "fileutils"
-require "stringio"
-require "canopus"
-require "canopus/cli"
 require "wezen"
 require "zaniah"
 
 FileUtils.mkdir_p("docs/media")
 png_path = "docs/media/overview.png"
-status = Canopus::CLI.main(["--headless", png_path, "README.md"], output: StringIO.new, error: $stderr)
-raise "Canopus headless demo failed (#{status})" unless status.zero?
+FileUtils.cp("assets/canopus-editor.png", png_path)
 
 width, height, rgba = Zaniah::PNG.decode(File.binread(png_path))
 animation = Wezen::Animation.new(width: width, height: height).add(rgba, delay_ms: 1_000)
