618bdb5638eb957e93222104b3728e5a22d05187
ydah <t.yudai92@gmail.com>
2026-09-20 20:53:39 +0900

    Keep terminal cast at demo duration

--- demo/02_terminal_cast.rb
+++ demo/02_terminal_cast.rb
@@ -9,7 +9,8 @@ events = [
   Wezen::Cast::Event.new(0.8, :output, "\e[32m$\e[0m bundle exec rake test\r\n"),
   Wezen::Cast::Event.new(1.2, :output, "\e[90mRunning deterministic checks...\e[0m\r\n"),
   Wezen::Cast::Event.new(2.0, :output, "\e[32mPASS\e[0m  128 tests, 0 failures\r\n"),
-  Wezen::Cast::Event.new(2.4, :output, "\r\n\e[36mready\e[0m _")
+  Wezen::Cast::Event.new(2.4, :output, "\r\n\e[36mready\e[0m _"),
+  Wezen::Cast::Event.new(10.0, :output, "\r\n")
 ]
 FileUtils.mkdir_p("docs/media")
 Wezen::Cast.write("docs/media/terminal.cast", width: 100, height: 30, title: "Canopus terminal", events: events,
